9c26a99753bae8547d6ad162b2da3a722a2e2229
Luciusnextech <jlucios@bol.com.br>
2026-06-24 12:29:03 -0300

    Delete ordem_servico directory

--- ordem_servico/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import OrdemServico, ImagemOS, ArquivoTecnico
-
-admin.site.register(OrdemServico)
-admin.site.register(ImagemOS)
-admin.site.register(ArquivoTecnico)
